3a1557cae456d5b39f799103772f24a0cc934b25
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2014-12-01 22:04:54 +0800

    index_refs.sh now executes commands instead of just printing them

--- bin/index_refs.sh
+++ bin/index_refs.sh
@@ -6,7 +6,7 @@ NCBI_GENOMES_DIR=/mnt/genomeDB/ncbi/genomes/Bacteria/
 test -d $NCBI_GENOMES || exit 1
 
 for fa in $(ls $OUT_DIR/*all.fa); do
-cat <<EOF
+#cat <<EOF
     bwa index $fa >/dev/null && samtools faidx $fa >/dev/null;
-EOF
+#EOF
 done
